56e24e705604b83f25f6a1cb0ce70d4e1aa8281e
Cristina Rojas Zamora <criss@Cristinas-MacBook-Pro-2.local>
2017-07-18 22:09:49 -0700

    Adding example of life time snack with js

---
+++ lifeTime.js
@@ -0,0 +1,20 @@
+(function(){
+	var currentAge = 28;
+	var maximunAge = 100;
+	var ammountDay = 2;
+
+	var total = (ammountDay * 365) * (maximunAge - currentAge);
+
+	// I will nedd 52560 Snickers to last until ripe old age of 100
+	console.log('I will nedd ' + total + ' Snickers to last until ripe old age of ' + maximunAge);
+})();
+
+
+
+/* Ever wonder how much a "lifetime supply" of your favorite snack is? Wonder no more!
+
+Store your current age into a variable.
+Store a maximum age into a variable.
+Store an estimated amount per day (as a number).
+Calculate how many you would eat total for the rest of your life.
+Output the result to the screen like so: "You will need NN to last you until the ripe old age of X". */
